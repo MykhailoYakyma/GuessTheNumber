91ef2e9e82a0644e7729f51daff9297133a18cfb
Mykhailo Yakyma <mexayakima1@gmail.com>
2021-10-08 16:55:55 +0200

    dialog update

--- app/src/main/java/com/example/guessthenumber/MainActivity.java
+++ app/src/main/java/com/example/guessthenumber/MainActivity.java
@@ -2,38 +2,91 @@ package com.example.guessthenumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int intentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int result;
-        int attempts = 0;
-        int random = (int)(Math.random() * 99 + 1);
-        EditText user_data = findViewById(R.id.user_number);
 
+        int number = (int)(Math.random()*99+1);
+        intentos = 0;
 
-
+        final EditText user_number = (EditText) findViewById(R.id.user_number);
         final Button button = findViewById(R.id.check_button);
+        final TextView historial = (TextView) findViewById(R.id.historial);
+        final TextView atempts_number = (TextView) findViewById(R.id.Atempts_number);
+        historial.setMovementMethod(new ScrollingMovementMethod());
+        atempts_number.setText(String.valueOf(intentos));
+
         button.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
 
-                int user_number = Integer.parseInt(user_data.getText().toString());
+                try{
+                    int number_user = Integer.parseInt(user_number.getText().toString());
+                    user_number.getText().clear();
 
-                Context context = getApplicationContext();
-                CharSequence text = "EXPLOOOOOOOOSION!";
-                int duration = Toast.LENGTH_SHORT;
+                    if (number==number_user){
+                        historial.append("Numero Correcto!");
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+
+                        dialog.setTitle("Player Name");
+                        dialog.setMessage("Write the nickname to be showed in the rancking:");
+
+                        final EditText dialog_input = new EditText(MainActivity.this);
+                        dialog_input.setWidth(10);
+                        dialog.setView(dialog_input);
+
+                        dialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                Editable user_name = dialog_input.getText();
+
+                            }
+                        });
+
+                        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+                            }
+                        });
+
+                        dialog.show();
+                        intentos = 0;
+
+                    }else{
+                        intentos++;
+                        Log.d("Numero: ", String.valueOf(number));
+                        atempts_number.setText(String.valueOf(intentos));
+                        if (number_user>number){
+                            historial.append("El numero es mas pequiño que "+ number_user + "\n");
+
+                        }else{
+                            historial.append("El numero es mas grande que "+ number_user + "\n");
+                        }
+
+                    }
+                }catch(Exception E){
+                    Log.i("siiiiiii","");
+                    Context context = getApplicationContext();
+                    Toast.makeText(context,"No has introducido el numero",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
